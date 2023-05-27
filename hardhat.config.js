90a75f729b4b8800bce694a176ad822aea80f240
Samuel Sope <sopesamuel@gmail.com>
2023-05-27 14:07:46 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,62 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      chainId: 11155111,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+
+  solidity: "0.8.7",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 200000,
+  },
+}
